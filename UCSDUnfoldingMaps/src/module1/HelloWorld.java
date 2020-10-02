e28988b18c4fe96005a3b7d46119278850c0b8b5
Camila Paschini <camila.paschini@gmail.com>
2020-10-02 14:34:39 +0200

    assignment done

--- UCSDUnfoldingMaps/src/module1/HelloWorld.java
+++ UCSDUnfoldingMaps/src/module1/HelloWorld.java
@@ -11,8 +11,8 @@ import de.fhpotsdam.unfolding.utils.MapUtils;
 /** HelloWorld
   * An application with two maps side-by-side zoomed in on different locations.
   * Author: UC San Diego Coursera Intermediate Programming team
-  * @author Your name here
-  * Date: July 17, 2015
+  * @author Camila Paschini
+  * Date: October 2, 2020
   * */
 public class HelloWorld extends PApplet
 {
@@ -30,24 +30,17 @@ public class HelloWorld extends PApplet
 	private static final boolean offline = false;
 	
 	/** The map we use to display our home town: La Jolla, CA */
-	UnfoldingMap map1;
+	UnfoldingMap mapLaJolla;
 	
 	/** The map you will use to display your home town */ 
-	UnfoldingMap map2;
+	UnfoldingMap mapStockholm;
 
 	public void setup() {
-		size(800, 600, P2D);  // Set up the Applet window to be 800x600
-		                      // The OPENGL argument indicates to use the 
-		                      // Processing library's 2D drawing
-		                      // You'll learn more about processing in Module 3
+		size(950, 600, P2D);
+		this.background(33, 33, 33);
 
-		// This sets the background color for the Applet.  
-		// Play around with these numbers and see what happens!
-		this.background(200, 200, 200);
-		
-		// Select a map provider
 		AbstractMapProvider provider = new Google.GoogleTerrainProvider();
-		// Set a zoom level
+
 		int zoomLevel = 10;
 		
 		if (offline) {
@@ -57,33 +50,21 @@ public class HelloWorld extends PApplet
 			// 3 is the maximum zoom level for working offline
 			zoomLevel = 3;
 		}
-		
-		// Create a new UnfoldingMap to be displayed in this window.  
-		// The 2nd-5th arguments give the map's x, y, width and height
-		// When you create your map we want you to play around with these 
-		// arguments to get your second map in the right place.
-		// The 6th argument specifies the map provider.  
-		// There are several providers built-in.
-		// Note if you are working offline you must use the MBTilesMapProvider
-		map1 = new UnfoldingMap(this, 50, 50, 350, 500, provider);
 
-		// The next line zooms in and centers the map at 
-	    // 32.9 (latitude) and -117.2 (longitude)
-	    map1.zoomAndPanTo(zoomLevel, new Location(32.9f, -117.2f));
-		
-		// This line makes the map interactive
-		MapUtils.createDefaultEventDispatcher(this, map1);
-		
-		// TODO: Add code here that creates map2 
-		// Then you'll modify draw() below
+		mapLaJolla = new UnfoldingMap(this, 50, 50, 350, 500, provider);
+	    mapLaJolla.zoomAndPanTo(zoomLevel, new Location(32.9f, -117.2f));
+		MapUtils.createDefaultEventDispatcher(this, mapLaJolla); // This line makes the map interactive
 
+		mapStockholm = new UnfoldingMap(this, 450, 50, 350, 500, provider);
+		mapStockholm.zoomAndPanTo(zoomLevel, new Location(59.3f, 18.0f));
+		MapUtils.createDefaultEventDispatcher(this, mapStockholm);
 	}
 
 	/** Draw the Applet window.  */
 	public void draw() {
 		// So far we only draw map1...
-		// TODO: Add code so that both maps are displayed
-		map1.draw();
+		mapLaJolla.draw();
+		mapStockholm.draw();
 	}
 
 	
